abadc7f9ab21925dd205e3ec0e3780d2eb011975
Steven Nguyen <steventn96@gmail.com>
2023-09-22 20:26:57 -0700

    added skeleton dao classes

---
+++ src/main/java/appointmentscheduler/dao/ContactDao.java
@@ -0,0 +1,13 @@
+package appointmentscheduler.dao;
+
+import appointmentscheduler.model.Contacts;
+import appointmentscheduler.helper.DBConnection;
+import appointmentscheduler.helper.Query;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ContactDao {
+}
